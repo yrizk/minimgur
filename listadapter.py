f897ad562d85ddd55b8a18e23ccd5e12cf830438
yohan rizk <yrizk@google>
2017-09-13 21:58:25 -0400

    updates to the thing

--- listadapter.py
+++ listadapter.py
@@ -7,12 +7,14 @@ class ListAdapter():
 		self.root = json.loads(jsonString);
 		if (self.root['status'] is not 200 or self.root['success'] is not True):
 			print('failure in network')
-			raise MainFeedNetworkFailureException() # do I need to declare that I'm throwing this? also TODO: catch error in main.py and return error message
+			raise NetworkException() # do I need to declare that I'm throwing this? also TODO: catch error in main.py and return error message
 		else:
 			self.data_list = []
-			for item in self.root['data']:
+			for x, item in enumerate(self.root['data']):
+				if x > 30:
+					break
 				self.data_list.append(MinimgurItem(item))
-				print('size: ' + str(len(self.data_list)))
+			print('deserialization complete')
 
 	def render(self):
 		return render_template('main.html', minimgur_list=self.data_list)
@@ -23,3 +25,9 @@ class MinimgurItem():
 		self.id = jsonObj['id']
 		self.title = jsonObj['title']
 		self.img = jsonObj['link']
+
+
+class NetworkError(SystemError):
+	def __init__(self):
+		pass
+
--- main.py
+++ main.py
@@ -1,28 +1,28 @@
-from flask import Flask
+from flask import Flask, g, render_template
 import requests
-from listadapter import ListAdapter
+from listadapter import ListAdapter, NetworkError
 app = Flask(__name__)
  
-CLIENT_ID = "5b0c23ba1e9064e"
+CLIENT_ID = "7dfb3564e49a8bd"
+CLIENT_SECRET = "33dee0351d4543dbd49bdea44c461aa371c483ee"
 
-@app.route("/")
+MAIN_URL = "https://api.imgur.com/3/gallery/{0}/{1}/{2}/{3}"
+
+@app.route("/", methods=['GET'])
 def index():
-	# TODO: if not logged in, rediret to login page
-	r = requests.get("https://api.imgur.com/3/gallery/hot/viral/0/top", headers={"authorization": "Client-ID " + CLIENT_ID})
-	listAdapter = ListAdapter(r.content)
-	return listAdapter.render()
+	# TODO: if not logged in, redirect to login page
+	r = requests.get(MAIN_URL.format("hot", "viral", "0", "top"), headers={"authorization": "Client-ID " + CLIENT_ID})
+	try:
+		listAdapter = ListAdapter(r.content)
+		return listAdapter.render()
+	except NetworkError:
+		return "Problem with fetching imgur feed"
  
-@app.route("/hello")
-def hello():
-    return "Hello World!"
- 
-@app.route("/members")
-def members():
-    return "Members"
- 
-@app.route("/members/<string:name>/")
-def getMember(name):
-    return name
- 
+@app.route("/login", methods=['GET'])
+# @templated('login.html') does not work :(
+def login():
+    return render_template('login.html', value="42")
+  
+
 if __name__ == "__main__":
     app.run()
